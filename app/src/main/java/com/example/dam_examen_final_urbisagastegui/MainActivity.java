8befa5f61f0e81f970611b44977c310dd86cb043
Jhon Antony Urbisagastegui Huamani <2113050227@untels.edu.pe>
2024-07-17 10:06:29 -0500

    4 commit avance

--- app/src/main/java/com/example/dam_examen_final_urbisagastegui/MainActivity.java
+++ app/src/main/java/com/example/dam_examen_final_urbisagastegui/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.dam_examen_final_urbisagastegui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Definir el evento click sobre el botón Login
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+            }
+
+        });
+
     }
 }
